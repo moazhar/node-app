63996c39bec626aa80ae31d86d7de04c82bbeedd
mohammed azhar <muhammedazhu39@gmail.com>
2022-02-05 13:01:14 +0530

    simple node app with test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var request = require('supertest');
+var app = require('../index');
+
+describe('GET /', function () {
+  it('respond with hello world', function (done) {
+    //navigate to root and check the response is "hello world"
+    request(app).get('/').expect('Hello world!', done);
+  });
+});
