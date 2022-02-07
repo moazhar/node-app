e415ba67d97371d50dda509dc387838296d69cfe
mohammed azhar <muhammedazhu39@gmail.com>
2022-02-07 10:30:19 +0530

    Updated deploy script

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-  res.send('Hello world!');
+  res.send('Hey Mohammed! How are you doing?');
 });
 
 //listen to port 3000 by default
--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@ var request = require('supertest');
 var app = require('../index');
 
 describe('GET /', function () {
-  it('respond with hello world', function (done) {
+  it('respond with Hey Mohammed! How are you doing?', function (done) {
     //navigate to root and check the response is "hello world"
-    request(app).get('/').expect('Hello world!', done);
+    request(app).get('/').expect('Hey Mohammed! How are you doing?', done);
   });
 });
